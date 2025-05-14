2b775e034ee6e955a0503a5556fa9a380f1ab5da
Your Name <pc@pc.com>
2025-05-14 13:08:59 +0900

    je

--- HelloWorld/src/com/yedam/JSExe.java
+++ HelloWorld/src/com/yedam/JSExe.java
@@ -1,22 +1,21 @@
 package com.yedam;
 
 public class JSExe {
+	
 	public static void main(String[] args) {
-		//변경된 부분
-		//ㅠ
-		//66666
-		//개어렵
-		//ㅈ45ㅈ4ㄷ6ㅅㄷ46ㅅ
-		//임의의 수를 생성.1 ~ 100사이의 임의의 값 생성
-		//1 <= x <11
-		int sum = 0;
-		for(int i=1; i<=5; i++) {
-	int result = (int)(Math.random()* 100) + 1; // 0 <= x < 1
-		sum += result;
-		}
-		System.out.println("결과: " +sum);
-		// end of main
-	}
+		int num1 = 30;
+		int num2 = 30;
+		System.out.println(num1 == num2);
+		
+		String str1 =new String("Hello");
+		String str2 =new String("Hello");
+		System.out.println(str1);
+		System.out.println(str2);
+		
+		//문자열을 비교할 경우 equals 사용
+		System.out.println(str1.equals(str2)); 
+		
+	} //end of main
 	
 	public static void test() {
 	int sum = 0;
@@ -30,8 +29,18 @@ public class JSExe {
 }		// end of test
 
 }	
-	
 
+
+//임의의 수를 생성.1 ~ 100사이의 임의의 값 생성
+//		//1 <= x <11
+//		int sum = 0;
+//		for(int i=1; i<=5; i++) {
+//	int result = (int)(Math.random()* 100) + 1; // 0 <= x < 1
+//		sum += result;
+//		}
+//		System.out.println("결과: " +sum);
+//		// end of main
+	
 	
 	
 	
